9d58162879646e357fc791519387307da2389aa7
Christian Borutta <ch.bo@web.de>
2016-01-05 14:04:41 +0100

    fixed typo

--- src/Pdf.js
+++ src/Pdf.js
@@ -27,7 +27,7 @@ class Pdf extends Component {
     if (pdf && ((newProps.page && newProps.page !== this.props.page) ||
       (newProps.scale && newProps.scale !== this.props.scale))) {
       this.setState({page: null});
-      pdf.getPage(newProps.page).then(this.onPageComplete)
+      pdf.getPage(newProps.page).then(this.onPageComplete);
     }
   }
 
