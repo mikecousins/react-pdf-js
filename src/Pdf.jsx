d13e3ecc1b34f3952d694e190a9efe369fc56ac5
Mike Cousins <mike@mikecousins.com>
2018-06-11 16:43:38 -0600

    Switching to latest pdfjs and away from pdf.combined to reduce size

--- src/Pdf.jsx
+++ src/Pdf.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-require('pdfjs-dist/build/pdf.combined');
+require('pdfjs-dist/build/pdf');
 
 const makeCancelable = (promise) => {
   let hasCanceled = false;
