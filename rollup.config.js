016c13ab41d100f0e568750e4f05bd2be7bdbe0f
Mike Cousins <mike@mikecousins.com>
2018-06-12 16:15:51 -0600

    Removing CSS

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import url from 'rollup-plugin-url';
+import pkg from './package.json';
 
-import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
@@ -21,9 +20,6 @@ export default {
   ],
   plugins: [
     external(),
-    postcss({
-      modules: true
-    }),
     url(),
     babel({
       exclude: 'node_modules/**'
