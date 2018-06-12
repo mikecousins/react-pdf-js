85b03c602cfdaf107a4c8b8d09e84dc08a5644ec
Mike Cousins <mike@mikecousins.com>
2018-06-12 00:00:34 -0600

    Super basic new library working

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-pdf-js'
+import Pdf from 'react-pdf-js'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Pdf file='test.pdf' />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 /**
- * @class ExampleComponent
+ * @class ReactPdfJs
  */
-
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import PdfJsLib from 'pdfjs-dist';
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
+export default class ReactPdfJs extends Component {
   static propTypes = {
-    text: PropTypes.string
+    file: PropTypes.string
+  }
+
+  componentDidMount() {
+    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.550/pdf.worker.js';
+    PdfJsLib.getDocument(this.props.file).then((pdf) => {
+      pdf.getPage(1).then((page) => {
+        var scale = 1.5;
+        var viewport = page.getViewport(scale);
+        
+        var canvas = document.getElementById('pdf-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        page.render(renderContext);
+      });
+    });
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return <canvas id="pdf-canvas" />;
   }
 }
