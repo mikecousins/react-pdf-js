8d0b7e11c4e12960fbe6cb795785f77680cb7c90
Rafał Kwaśniewski <rafal.kwasniewski@netguru.com>
2019-02-19 18:08:06 +0700

    back to 2.0.943 version

--- src/index.js
+++ src/index.js
@@ -42,9 +42,9 @@ export default class ReactPdfJs extends Component {
       cMapUrl,
       cMapPacked,
     } = this.props;
-    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.550/pdf.worker.js';
+    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js';
     PdfJsLib.getDocument({ url: file, cMapUrl, cMapPacked }).then((pdf) => {
-      this.setState({ pdf, numPages: pdf.pdfInfo.numPages });
+      this.setState({ pdf, numPages: pdf._pdfInfo.numPages }); // eslint-disable-line
 
       pdf.getPage(page).then(p => this.drawPDF(p));
     });
