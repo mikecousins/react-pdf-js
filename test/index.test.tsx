97f1f07d9adf90e2952fbcccd6395e2fb17149ec
Mike Cousins <mike@mikecousins.com>
2019-06-19 15:17:47 -0600

    Updating packages

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 
-import Pdf from '../src';
+import { usePdf } from '../src/index';
 
 describe('Pdf', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    render(<Pdf file="test.pdf" />, div);
+    const canvasEl = useRef(null);
+
+    usePdf({
+      file: 'basic.33e35a62.pdf',
+      page: 1,
+      canvasEl,
+    });
+    render(<canvas ref={canvasEl} />, div);
     unmountComponentAtNode(div);
   });
 });
