e4c5ae46898e36895b4fc8b5ab985a7991395052
Mike Cousins <mike@mikecousins.com>
2022-07-08 17:20:06 -0600

    Adding type generation

--- packages/react-pdf-js/vite.config.js
+++ packages/react-pdf-js/vite.config.js
@@ -1,12 +1,29 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path  from'path';
+import { defineConfig } from 'vite';
 
 module.exports = defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.tsx'),
       name: '@mikecousins/react-pdf',
       fileName: (format) => `mikecousins-react-pdf.${format}.js`
     },
-  }
-})
+  },
+  rollupOptions: {
+    external: ['react', 'react-dom'],
+    output: {
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+    },
+  },
+});
