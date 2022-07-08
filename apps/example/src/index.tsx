ea26bfadd766a27f45e7d9e81acc174bf5b1141d
Mike Cousins <mike@mikecousins.com>
2022-07-08 17:20:17 -0600

    Removing component

--- apps/example/src/index.tsx
+++ apps/example/src/index.tsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Pdf } from '@mikecousins/react-pdf';
+import { usePdf } from '@mikecousins/react-pdf';
 
 const App = () => {
   const [page, setPage] = useState(1);
+  const canvasRef = useRef(null);
+
+  const { pdfDocument } = usePdf({
+    file: 'test.pdf',
+    page,
+    canvasRef,
+  });
 
   return (
-    <Pdf file="basic.pdf" page={page}>
-      {({ pdfDocument, canvas }) => (
-        <>
-          {!pdfDocument && <span>Loading...</span>}
-          {canvas}
-          {Boolean(pdfDocument?.numPages) && (
-            <nav>
-              <ul className="pager">
-                <li className="previous">
-                  <button
-                    disabled={page === 1}
-                    onClick={() => setPage(page - 1)}
-                  >
-                    Previous
-                  </button>
-                </li>
-                <li className="next">
-                  <button
-                    disabled={page === pdfDocument?.numPages}
-                    onClick={() => setPage(page + 1)}
-                  >
-                    Next
-                  </button>
-                </li>
-              </ul>
-            </nav>
-          )}
-        </>
+    <div>
+      {!pdfDocument && <span>Loading...</span>}
+      <canvas ref={canvasRef} />
+      {Boolean(pdfDocument && pdfDocument.numPages) && (
+        <nav>
+          <ul className="pager">
+            <li className="previous">
+              <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+                Previous
+              </button>
+            </li>
+            <li className="next">
+              <button
+                disabled={page === pdfDocument.numPages}
+                onClick={() => setPage(page + 1)}
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
       )}
-    </Pdf>
+    </div>
   );
 };
 
