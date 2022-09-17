44007f7ccacbb773f76571c4cc7ab395c545dc12
Mike Cousins <mike@mikecousins.com>
2022-09-16 20:21:01 -0600

    Updating demo site

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,7 +1,11 @@
 import { useRef, useState } from 'react';
 import { usePdf } from '@mikecousins/react-pdf';
-import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/24/outline';
+import {
+  ArrowLeftCircleIcon,
+  ArrowRightCircleIcon,
+} from '@heroicons/react/24/outline';
 import './index.css';
+import clsx from 'clsx';
 
 function App() {
   const [page, setPage] = useState(1);
@@ -11,37 +15,95 @@ function App() {
     file: 'udm_se_ds.pdf',
     page,
     canvasRef,
-    scale: 0.6,
+    scale: 0.4,
   });
+
+  const previousDisabled = page === 1;
+  const nextDisabled = Boolean(page === pdfDocument?.numPages);
 
   return (
     <div className="w-full flex flex-col">
-      <div className="bg-gradient-to-r from-sky-800 to-indigo-800 container text-center py-12">
-        <div className="text-4xl font-bold text-white">@mikecousins/react-pdf</div>
-        <div className="text-xl text-gray-200 mt-4">The easiest way to render PDFs in React</div>
+      <div className="bg-gradient-to-r from-sky-800 to-indigo-800">
+        <div className="container text-center py-12 mx-auto">
+          <div className="text-4xl font-bold text-white">
+            @mikecousins/react-pdf
+          </div>
+          <div className="text-xl text-gray-200 mt-4">
+            The easiest way to render PDFs in React.{' '}
+            <a
+              href="https://bundlephobia.com/package/@mikecousins/react-pdf"
+              className="text-blue-400"
+            >
+              Under 1kB in size.
+            </a>{' '}
+            Modern React hook architecture.
+          </div>
+        </div>
       </div>
-      <div className="bg-gray-100 container text-center py-12">
-        <div className="flex">
-          <div className="flex-1">
-            <button disabled={page === 1} onClick={() => setPage(page - 1)}>
-              <ArrowLeftCircleIcon className="h-16 w-16" />
-            </button>
+      <div className="bg-gray-100">
+        <div className="container text-center py-12 mx-auto flex">
+          <div className="flex-1 flex flex-col">
+            <div className="flex-1" />
+            <div className="text-center">
+              <button
+                disabled={previousDisabled}
+                onClick={() => setPage(page - 1)}
+                className={clsx(previousDisabled && 'text-gray-300')}
+              >
+                <ArrowLeftCircleIcon className="h-12 w-12" />
+              </button>
+            </div>
+            <div className="flex-1" />
           </div>
           <div>
             {!pdfDocument && <span>Loading...</span>}
             <canvas ref={canvasRef} />
           </div>
-          <div className="flex-1">
-            <button
-              disabled={page === pdfDocument?.numPages}
-              onClick={() => setPage(page + 1)}
-            >
-              <ArrowRightCircleIcon className="h-16 w-16" />
-            </button>
+          <div className="flex-1 flex flex-col">
+            <div className="flex-1" />
+            <div className="text-center">
+              <button
+                disabled={nextDisabled}
+                onClick={() => setPage(page + 1)}
+                className={clsx(nextDisabled && 'text-gray-300')}
+              >
+                <ArrowRightCircleIcon className="h-12 w-12" />
+              </button>
+            </div>
+            <div className="flex-1" />
           </div>
         </div>
       </div>
-      <div className="bg-gray-200 container text-center py-12">Code example coming soon!</div>
+      <div className="bg-gray-200">
+        <div className="container text-center py-12 mx-auto">
+          <div className="flex flex-row gap-4">
+            <a
+              href="https://www.npmjs.com/package/@mikecousins/react-pdf"
+              className="text-blue-400 underline"
+            >
+              NPM
+            </a>
+            <a
+              href="https://github.com/mikecousins/react-pdf-js"
+              className="text-blue-400 underline"
+            >
+              Github
+            </a>
+            <a
+              href="https://bundlephobia.com/package/@mikecousins/react-pdf"
+              className="text-blue-400 underline"
+            >
+              Bundlephobia
+            </a>
+            <a
+              href="https://opencollective.com/react-pdf-js"
+              className="text-blue-400 underline"
+            >
+              Open Collective
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
