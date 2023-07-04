c900aaf6adf58665ce3d6b2b03ae6db6b4d56b8c
Karim Dallas-Newell <22102599+TheNewLad@users.noreply.github.com>
2023-07-03 19:45:44 -0500

    refactor: make drawPdf function less opinionated
    I think drawPdf should leave the styling and scale of the pdf to the hook caller.  
    
    Setting the canvas height and width is fine for displaying the PDF since the canvas is taking on the height and width of the PDF. The canvas element that uses the hook can be styled in any fashion through class name or style.
    
    The default scale of 1 is acceptable for any screen. Making the caller of the hook responsible for the scale allows for better usability of the hook in many cases.

--- packages/react-pdf-js/src/index.tsx
+++ packages/react-pdf-js/src/index.tsx
@@ -117,9 +117,7 @@ export const usePdf = ({
       // Because this page's rotation option overwrites pdf default rotation value,
       // calculating page rotation option value from pdf default and this component prop rotate.
       const rotation = rotate === 0 ? page.rotate : page.rotate + rotate;
-      const dpRatio = window.devicePixelRatio;
-      const adjustedScale = scale * dpRatio;
-      const viewport = page.getViewport({ scale: adjustedScale, rotation });
+      const viewport = page.getViewport({ scale, rotation });
       const canvasEl = canvasRef!.current;
       if (!canvasEl) {
         return;
@@ -129,9 +127,7 @@ export const usePdf = ({
       if (!canvasContext) {
         return;
       }
-
-      canvasEl.style.width = `${viewport.width / dpRatio}px`;
-      canvasEl.style.height = `${viewport.height / dpRatio}px`;
+      
       canvasEl.height = viewport.height;
       canvasEl.width = viewport.width;
 
