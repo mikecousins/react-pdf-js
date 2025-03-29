cdcda2ffca13ba73ac0f0c5743bdd73d8e5b14e3
Mike Cousins <mikecousins@gmail.com>
2025-03-28 22:31:50 -0600

    Updating github link

--- apps/docs/src/components/Layout.tsx
+++ apps/docs/src/components/Layout.tsx
@@ -57,7 +57,7 @@ function Header() {
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:grow">
         <ThemeSelector className="relative z-10" />
-        <Link href="https://github.com" className="group" aria-label="GitHub">
+        <Link href="https://github.com/mikecousins/react-pdf-js" className="group" aria-label="GitHub">
           <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
         </Link>
       </div>
