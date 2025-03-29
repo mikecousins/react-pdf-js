4810dc0965a70b1e6b55c802c5d5963c55d7302e
Mike Cousins <mikecousins@gmail.com>
2025-03-29 14:04:17 -0600

    Updating title

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -24,7 +24,7 @@ const lexend = localFont({
 export const metadata: Metadata = {
   title: {
     template: '%s - Docs',
-    default: '@mikecousins/react-pdf - Never miss the cache again.',
+    default: '@mikecousins/react-pdf - Embed PDFs in React.',
   },
   description:
     'Cache every single thing your app could ever do ahead of time, so your code never even has to run at all.',
