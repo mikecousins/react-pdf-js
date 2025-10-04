b174e92add3f0f7c6a1d0586886a1c275157efe1
Mike Cousins <mikecousins@gmail.com>
2025-10-03 19:14:29 -0600

    Adding Netlify plugin

--- apps/demo-react-router/vite.config.ts
+++ apps/demo-react-router/vite.config.ts
@@ -1,8 +1,9 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
